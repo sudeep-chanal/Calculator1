8aef3d326ec5d95b3ede956bf9d449e553a2a6f8
sudeep-chanal <01fe22bca181@kletech.ac.in>
2024-03-17 22:13:31 +0530

    program

---
+++ Child.java
@@ -0,0 +1,13 @@
+public class Child {
+    float square(float operand_1) {
+        return (operand_1 * operand_1);
+    }
+
+    float cube(float operand_1) {
+        return (operand_1 * operand_1 * operand_1);
+    }
+
+    float mod(float operand_1, float operand_2) {
+        return (operand_1 % operand_2);
+    }
+}

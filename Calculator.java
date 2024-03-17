4f02e83375d4e69914c12c28c13d45f849940c0c
sudeep-chanal <01fe22bca181@kletech.ac.in>
2024-03-17 22:11:19 +0530

    Calculator program

---
+++ Calculator.java
@@ -0,0 +1,51 @@
+class Calculator extends Child {
+    float addition(float operand_1, float operand_2) {
+        return (operand_1 + operand_2);
+    }
+
+    float subtraction(float operand_1, float operand_2) {
+        return (operand_1 - operand_2);
+    }
+
+    float multiplication(float operand_1, float operand_2) {
+        return (operand_1 * operand_2);
+    }
+
+    float division(float operand_1, float operand_2) {
+        return (operand_1 / operand_2);
+    }
+
+    float square(float operand_1) {
+        return (operand_1 * operand_1);
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        float operand_1 = 10;
+        float operand_2 = 5;
+
+        float add_result = calc.addition(operand_1, operand_2);
+        System.out.println("result of subtraction is:" + add_result);
+
+        float sub_result = calc.subtraction(operand_1, operand_2);
+        System.out.println("result of subtraction is:" + sub_result);
+
+        float mul_result = calc.multiplication(operand_1, operand_2);
+        System.out.println("result of multiplication is:" + mul_result);
+
+        float div_result = calc.division(operand_1, operand_2);
+        System.out.println("result of division:" + div_result);
+
+        float sq_result = calc.square(operand_1);
+        System.out.println("result of square is:" + sq_result);
+
+        float cube_result = calc.cube(operand_1);
+        System.out.println("result of cube is:" + cube_result);
+
+        float modulus_result = calc.mod(operand_1, operand_2);
+        System.out.println("result of mod is:" + modulus_result);
+
+    }
+
+}
